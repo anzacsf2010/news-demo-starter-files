b2b00515c65e9978e6a66ff29db216e5db7be17b
Andy St Fort <andy.st.fort@niceincontact.com>
2023-08-16 23:22:13 -0400

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+	"github.com/joho/godotenv"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var temp = template.Must(template.ParseFiles("index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	temp.Execute(w, nil)
+}
+
+func main() {
+	_err := godotenv.Load()
+	if _err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		return
+	}
+}
