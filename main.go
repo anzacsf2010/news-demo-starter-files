3ebad3de255437d4afcb64b89d50892e4a59ade9
Andy St Fort <andy.st.fort@niceincontact.com>
2023-08-17 13:46:33 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .env
    	modified:   assets/style.css
    	modified:   index.html
    	modified:   main.go
    	modified:   news-demo-starter-files
    	new file:   news/news.go

--- main.go
+++ main.go
@@ -1,17 +1,102 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/freshman-tech/news-demo-starter-files/news"
 	"github.com/joho/godotenv"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"time"
 )
+
+type Search struct {
+	Query      string
+	NextPage   int
+	TotalPages int
+	Results    *news.Results
+}
 
 var temp = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	temp.Execute(w, nil)
+}
+
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query:      searchQuery,
+			NextPage:   nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results:    results,
+		}
+
+		if ok := !search.IsLastPage(); ok {
+			search.NextPage++
+		}
+
+		buf := &bytes.Buffer{}
+		err = temp.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = buf.WriteTo(w)
+		if err != nil {
+			return
+		}
+
+		fmt.Printf("%+v", results)
+		fmt.Println("Search query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+	}
+}
+
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
 }
 
 func main() {
@@ -25,8 +110,21 @@ func main() {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatalf("Env: API key (apiKey) must be set but is not. Please check and try again!")
+	}
 
+	myClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/search", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
 	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
